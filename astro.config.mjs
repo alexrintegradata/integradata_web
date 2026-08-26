b557903d05fe3bc2b721f201adc66ab079ba2e37
Alexis Reyes <alexis.reyes@integradata.cl>
2026-08-25 21:24:34 -0400

    mejora ux estilo apple y cambio de numero wsp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://integradata.cl",
+  integrations: [sitemap()],
   server: {
     host: true,
     port: 5173
